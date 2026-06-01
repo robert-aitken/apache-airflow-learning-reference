6fb82ea9d25e09cff48927dd913c55d965aba38b
robert-aitken <robertleighaitken@ymail.com>
2026-06-01 21:31:02 +0100

    Refactor XCom DAG from manual push/pull to TaskFlow return value passing

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,21 +1,21 @@
 # Share the value from task_a to task_b using XCom.
-from airflow.sdk import dag, task, Context
+from airflow.sdk import dag, task
 
 
 @dag
 def xcom_dag():
 
     @task
-    def task_a(**context: Context):
+    def task_a():
         val = 42
-        context["ti"].xcom_push(key="my_key", value=val)
+        return val
 
     @task
-    def task_b(**context: Context):
-        val = context["ti"].xcom_pull(task_ids="task_a", key="my_key")
+    def task_b(val: int):
         print(val)
 
-    task_a() >> task_b()
+    val = task_a()
+    task_b(val)
 
 
 xcom_dag()
