4b82e8b622f0469cdeb54b934a7dea0192399da8
robert-aitken <robertleighaitken@ymail.com>
2026-06-02 09:59:58 +0100

    Add S3 sensor DAG example

---
+++ dags/sensors_dag.py
@@ -0,0 +1,29 @@
+from airflow.sdk import dag, task
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from pendulum import datetime
+
+
+@dag(
+    schedule=None,
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    description="DAG showing how to wait for a file in S3 using a sensor",
+    tags=["sensors"],
+)
+def sensors_dag():
+
+    wait_for_file = S3KeySensor(
+        task_id="wait_for_file",
+        aws_conn_id="aws_s3",
+        bucket_key="s3://example-bucket/data_*",
+        wildcard_match=True,
+    )
+
+    @task
+    def process_file():
+        print("I processed the file!")
+
+    wait_for_file >> process_file()
+
+
+sensors_dag()
